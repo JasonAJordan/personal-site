578e51117782a0ea86a53b98bfedfff0b388d922
JasonAkiraJordan <Jasonajordan0@gmail.com>
2021-03-12 19:13:59 -0500

    starting

---
+++ src/componets/NavBar.js
@@ -0,0 +1,16 @@
+function NavBar() {
+    return (
+        <header>
+        Jason Jordan
+        <br/>
+        <a>Projects</a>
+        <a>About Me</a>
+        <a>Contact</a>
+        Still the header
+      </header>
+
+    );
+  }
+  
+  export default NavBar;
+  
